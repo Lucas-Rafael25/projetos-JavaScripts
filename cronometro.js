1ba473b011bd8ca5770287696075fed2c43110db
Lucas_Rafael <lucas.dev.br25@gmail.com>
2025-07-28 23:00:10 -0700

    correção de bugs

--- cronometro.js
+++ cronometro.js
@@ -4,41 +4,50 @@ var reset = document.getElementById('reset')
 var timer = document.getElementById('timer')
 var interval,time;
 var tempoDecorrido = 0
-function começar(pause1){
-    if(pause1 == 1){
+function começar(pause1=0,rec=0){
+    if(pause1 == 1 || start.value == 'cmc'){
+        start.value = 'comecei'
         time = Date.now() - tempoDecorrido
         interval = setInterval(() => {     
         let zero = Date.now() - time
         let hora = Math.floor(zero / 3600000)
-        let min = Math.floor((zero % 3600000)/ 60000) 
-        let seg = Math.floor((zero % 60000)/ 1000) 
+        let min = Math.floor((zero % 3600000) / 60000) 
+        let seg = Math.floor((zero % 60000) / 1000) 
         let mili = zero % 1000
         timer.innerHTML = `${hora}:${min}:${seg}:${mili}`
-        start.innerHTML = 'RESTART'
-        
         },16)
-        }else if(pause1 == 2){
-            clearInterval(interval)
-            tempoDecorrido = Date.now() - time
-        }else{
-            clearInterval(interval)
-            tempoDecorrido = 0
-            timer.innerHTML = '00:00:00.000'
-            start.innerHTML = 'START'
-            pause.innerHTML = 'PAUSE'
         }
-    
+        if(pause1 == 2 ){
+                clearInterval(interval)
+                tempoDecorrido = Date.now() - time
+        }
+        if(pause1==3){
+            if(start.value == 'comecei'){
+                clearInterval(interval)
+                tempoDecorrido = 0
+                timer.innerHTML = '00:00:00.000'
+                start.value = 'cmc'
+                pause.innerHTML = 'PAUSE'
+            }
+            
+        }
 }
 function pausar(){
-    if(pause.innerHTML == 'PAUSE'){
+    if(timer.innerHTML == '00:00:00.000'){
+        alert('O tempo está zerado, impossível pausar o cronômetro')
+    }else if(pause.innerHTML == 'PAUSE'){
         começar(2)
         pause.innerHTML = 'PAUSED'
     }else{
-        começar(1)
         pause.innerHTML = 'PAUSE'
+        começar(1)
     }
         
 }
 function resetar(){
-    começar(3)
+    if(timer.innerHTML == '00:00:00.000'){
+        alert('impossível resetar, o cronômetro não foi iniciado')
+        return
+    }else{começar(3)}
+    
 }
